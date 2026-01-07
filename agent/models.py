18a2d06d86085cfd0ff87b675effd9210aca6f66
anandpy2026 <anandpy2026@gmail.com>
2026-01-07 07:04:34 +0000

    all models are create

---
+++ agent/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Agent(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, unique=True)
+    referral_code = models.CharField(max_length=255, unique=True)
+    commission_percentage = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
